b52e7dda1f45a28ddf5c8df7a9ca63785e225361
loujonsson <loujonsson99@gmail.com>
2021-04-08 17:09:44 +0200

    main

---
+++ src/fileProcessor.erl
@@ -0,0 +1,19 @@
+%%%-------------------------------------------------------------------
+%%% @author lou
+%%% @copyright (C) 2021, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 08. Apr 2021 17:02
+%%%-------------------------------------------------------------------
+-module(fileProcessor).
+-author("lou").
+
+%% API
+-export([receiveFile/1]).
+
+receiveFile(File) ->
+  File, processHeader(File).
+
+processHeader(File) ->
+
---
+++ src/main.erl
@@ -0,0 +1,15 @@
+%%%-------------------------------------------------------------------
+%%% @author lou
+%%% @copyright (C) 2021, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 08. Apr 2021 17:00
+%%%-------------------------------------------------------------------
+-module(main).
+-author("lou").
+
+%% API
+-export([run/1]).
+
+run(File) -> fileProcessor:receiveFile(File).
