af0010cc084a2bbc006a296a2347b953d84723fc
A <Olegovish@hotmail.com>
2021-05-04 15:30:35 +0200

    added files and fixed benchmark
    -added all input files needed for benchmarking
    - fixed so that you can benchmark all input files agains eachother

--- src/benchmark.erl
+++ src/benchmark.erl
@@ -10,32 +10,43 @@
 -author("ant").
 
 %% API
--export([mainStatictics/0,mainTimer/0,timeForDatabase/0]).
+-export([mainStatictics/0,mainTimer/0,timeTaken/1,test/0,statsRun/1]).
 mainTimer() ->
   Test = timer:tc(?MODULE, timeForDatabase,[]),
   io:format("timer:took this amount of time  ~p~n",[Test]).
+
+timeTaken(Filename) ->
+  Start = os:timestamp(),
+  main:run(Filename),
+  timer:now_diff(os:timestamp(), Start) / 1000.
+
 mainStatictics() ->
+  avgTime= timeTaken,
+  io:format("average time for the run : ~n"),
+  io:format(" ~p ~n",[avgTime]).
+
+statsRun(Filename)->
   statistics(runtime),
-  timeForDatabase(),
+  main:run(Filename),
   {_,Time_Since_Last_Call} = statistics(runtime),
-  Time_In_Microsecounds=Time_Since_Last_Call*1000,
-%  io:format(" ~p ~p ~n",["1000","10000","100000","1000000","10000000"]),
-  io:format(" time for run was : ~n"),
-  io:format(" ~p ~n",[Time_In_Microsecounds]).
+  Time_Since_Last_Call.
 
 
-
-timeForDatabase() ->
-  %database read/write code hereh
-  main:run("input1000.txt").
 
 
 
 average(X) ->
   average(X, 0, 0).
-
 average([H|T], Length, Sum) ->
   average(T, Length + 1, Sum + H);
-
 average([], Length, Sum) ->
   Sum / Length.
+
+loopfunction(End, End, F,Args) -> [F(Args)];
+loopfunction(Start, End, F,Args) -> [F(Args)|loopfunction(Start+1, End, F,Args)].
+
+test() ->
+  io:format("avrage time for 1000 sample: ~f ~n",[average(loopfunction(0, 100, fun statsRun/1,"input1000.txt"))]),
+  io:format("avrage time for 10000 sample: ~f ~n",[average(loopfunction(0, 100, fun statsRun/1,"input10000.txt"))]),
+  io:format("avrage time for 100000 sample: ~f ~n",[average(loopfunction(0, 100, fun statsRun/1,"input100000.txt"))]),
+  io:format("done with benchmark").
