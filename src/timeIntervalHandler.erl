153a738dc17acad94aac755269fd76451ec96ba0
A <Olegovish@hotmail.com>
2021-05-18 01:19:07 +0200

    code from anton in timer interval handler

--- src/timeIntervalHandler.erl
+++ src/timeIntervalHandler.erl
@@ -19,3 +19,32 @@ repeatedOutputFile(startTime,intervalTime,stopTime) ->
 %will handle future call for sending outPutfile.
 featureHandler(Time) ->
   io:format("test").
+
+
+-define(INTERVAL, 60000). % One minute
+
+init(Args) ->
+  % Start first timer
+  MyRef = erlang:make_ref(),
+{ok, TRef} = erlang:send_after(?INTERVAL, self(), {trigger, MyRef}).
+
+
+% Trigger only when the reference in the trigger message is the same as in State
+handle_info({trigger, MyRef}, State = #your_record{latest=MyRef}) ->
+  % Do the action
+
+  % Start next timer
+  MyRef = erlang:make_ref(),
+{ok, TRef} = erlang:send_after(?INTERVAL, self(), trigger),
+
+
+% Ignore this trigger, it has been superceeded!
+handle_info({trigger, _OldRef}, State) ->
+  {ok, State}.
+
+handle_info(reset, State = #your_record{timer=TRef}) ->
+  % Cancel old timer
+  erlang:cancel_timer(TRef),
+  % Start next timer
+  MyNewRef = erlang:make_ref(),
+  {ok, NewTRef} = erlang:send_after(?INTERVAL, self(), trigger),
