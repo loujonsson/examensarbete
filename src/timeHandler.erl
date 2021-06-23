b04a632ae6174666ea88a67e005b98a8befe674c
A <Olegovish@hotmail.com>
2021-06-23 15:46:24 +0200

    Update timeHandler.erl

--- src/timeHandler.erl
+++ src/timeHandler.erl
@@ -31,3 +31,40 @@ timeConverter({{Year,Month,Day},{Hours,Minutes,Seconds}} = SpecifiedTime) ->
     SpecifiedTime
   ) - 62167219200)*1000000,
   TimeInMs.
+
+
+% period for IMSIstat 1000-3000
+%start ={{Year,Month,Day},{Hours,Minutes,Seconds}
+%stop = {{Year,Month,Day},{Hours,Minutes,Seconds}
+periodHandler()->
+startBreakPoint = io:get_line(io:format("Set start time for statistic break-point ~n")),
+stopBreakPoint = io:get_line(io:format("Set stop time for statistic break-point ~n")),
+outputFilePeriodFrequency = io:get_line(io:format("Set Period Frequency ~n")),
+startInterval = io:get_line(io:format("Set start time for interval~n")),
+stopInterval = io:get_line(io:format("Set stop time for interval ~n")),
+if (stopInterval > stopBreakPoint) ->
+io:format("stop inteval is out of breakpoint"),
+periodHandler();
+true -> continue
+end.
+
+
+init([]) ->
+  Timer = erlang:send_after(0, self(), check),
+  {ok, Timer}.
+
+handle_info(check, OldTimer) ->
+  erlang:cancel_timer(OldTimer),
+  Timer = erlang:send_after(10, self(), check),
+  do_task(), % A function that executes your task
+  {noreply, Timer}.
+
+
+Timer = erlang:send_after(outputFilePeriodFrequency, self(), check),
+{ok, Timer}.
+
+
+% period for output freq = everey 5
+%period start 1001
+%period stop 2005
+%function new output + restart the freq.
